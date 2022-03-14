183d8753942084cf7f937c5417ac13bed4ed5442
Marimarco <99329307+Marimarco@users.noreply.github.com>
2022-03-14 11:27:56 -0300

    Create codigo.ino

---
+++ exercicios/ex2/codigo.ino
@@ -0,0 +1,19 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(9, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(9, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+    digitalWrite(12, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(12, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
